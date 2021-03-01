3c6735718ae6c1d8ec51b396e8f5309503ca1477
Demayne Collins <collinsd73@gmail.com>
2021-03-01 15:35:21 -0500

    added variable animal inside main function

--- Reservations.java
+++ Reservations.java
@@ -1,5 +1,7 @@
 public class Reservations {
      public static void main(String[] args) {
+         String animal = "Goat";
+         System.out.println("I like to eat " + animal + " meat");
          
      } 
 }
