4ceccd44993cd354330381a148bbd6310f435dd2
Demayne Collins <collinsd73@gmail.com>
2021-03-01 17:52:32 -0500

    updated apply.py to fix bug inside of person object

--- apply.py
+++ apply.py
@@ -1,6 +1,6 @@
 
 def myName(first_name, last_name, age): 
-    person = "Hello, this is " + first_name + " " + last_name + " and I am " + age + " years old!"
+    person = "Hello, this is " + first_name + " " + last_name + " and I am " + str(age) + " years old!"
     return person 
     
 print(myName("James", "Bond", 67))
